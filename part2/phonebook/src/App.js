a08814667f3db5cd5fbbb8bc459c34acfbfd474a
Yewsh <66912003+EcoFiendly@users.noreply.github.com>
2023-03-18 09:02:42 +0000

    Part 2.6 done

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const Person = ({ name }) => {
+    // console.log(name)
+    return (
+      <div>{name}</div>
+    )
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={handlePersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <>
+        {persons.map(person =>
+          <Person key={person.name} name={person.name} />
+        )}
+      </>
+    </div>
+  )
+}
+
+export default App
