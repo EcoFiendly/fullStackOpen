725cb62fcd86114f24e2f427efbbab41560a04d2
EcoFiendly <yewsh.lim@gmail.com>
2023-03-21 20:22:53 +0000

    Part 2.9 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
@@ -39,9 +41,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const [filter, setFilter] = useState('')
+  
+  const filteredPersons = persons.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()))
+  
+  const handleFilter = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input type="text" value={filter} onChange={handleFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -59,8 +73,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <>
-        {persons.map(person =>
-          <Person key={person.name} name={person.name} number={person.number} />
+        {filteredPersons.map(person =>
+          <Person key={person.id} name={person.name} number={person.number} />
         )}
       </>
     </div>
